281817dc389ce02bafc9a21822343e3ee9397a32
paulcitronico <paulcitronico@gmail.com>
2026-03-31 17:18:05 -0300

    cambio de direcion den la bd

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-super-secreta-cambiala-en-produccion'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'condo_admin.db')
+        'sqlite:///' + os.path.join(basedir, 'app/instance/condo_admin.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Configuración de sesiones
