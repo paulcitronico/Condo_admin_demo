2409439dcef4d31c7b02cc1d36b38ed876b50740
paulcitronico <52328285+paulcitronico@users.noreply.github.com>
2026-03-31 17:52:39 -0300

    Add database initialization in app context

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from app import create_app, db
 
 # Crear aplicación
 app = create_app(os.getenv('FLASK_ENV') or 'development')
-
+with app.app_context():
+    db.create_all()
 
 @app.shell_context_processor
 def make_shell_context():
