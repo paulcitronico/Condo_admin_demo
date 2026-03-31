0e252a653d2aa050134137438de10877550b3527
paulcitronico <paulcitronico@gmail.com>
2026-03-31 19:03:06 -0300

    cambio de host

--- run.py
+++ run.py
@@ -13,4 +13,4 @@ def init_db():
 if __name__ == '__main__':
     # Desarrollo local - puerto 5000
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
